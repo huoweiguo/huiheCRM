d1e96931ff8389812466fa0ece387f9816abbc5d
lihy <479140237@qq.com>
2024-04-29 10:39:53 +0800

    修改计算

--- src/views/ProjectManager/components/blocks/Luru.vue
+++ src/views/ProjectManager/components/blocks/Luru.vue
@@ -389,7 +389,7 @@ const calculate = (row: itemType) => {
     unitPriceExcludingTax: row.unitPriceIncludingTax / 1.13,
 
     // 未税成本总价=未税成本单价*数量
-    totalCostExcludingTax: parseFloat((row.unitPriceIncludingTax / 1.13).toFixed(2)) * (row.productNum || 1),
+    totalCostExcludingTax: (row.unitPriceIncludingTax / 1.13) * (row.productNum || 1),
 
     // 含税成本总价=含税成本单价*数量
     totalCostIncludingTax: row.unitPriceIncludingTax * (row.productNum || 1),
